76e5aa69280e3b1ed1f0c1087fe5ce89ef892480
TOYODA Tetsuya <aql@hikoboshi.org>
2015-06-12 07:09:38 +0900

    Fix indents and tabs

--- dr.near/www/js/app.js
+++ dr.near/www/js/app.js
@@ -1,192 +1,192 @@
 if ( typeof( APP_CONFIG ) == 'undefined' ) {
-	APP_CONFIG = {};
+    APP_CONFIG = {};
 }
 
 angular.module('DrNEAR', ['ionic', 'DrNEAR.controllers'])
     .run(function($ionicPlatform) {
-		Parse.initialize( APP_CONFIG.PARSE_APP_KEY, APP_CONFIG.PARSE_APP_SECRET )
-		$ionicPlatform.ready(function() {
-			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		    // for form inputs)
-		    if (window.cordova && window.cordova.plugins.Keyboard) {
-			    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		    }
-		    if (window.StatusBar) {
-			    // org.apache.cordova.statusbar required
-			    StatusBar.styleDefault();
-		    }
-	    });
+        Parse.initialize( APP_CONFIG.PARSE_APP_KEY, APP_CONFIG.PARSE_APP_SECRET )
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
     .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, USER_ROLES) {
-	    openFB.init({appId: APP_CONFIG.FACEBOOK_APP_ID});
-	    $ionicConfigProvider.views.maxCache(0);
-	    $stateProvider
-	        .state('app', {
-		        url: '/app',
-		        templateUrl: 'templates/menu.html',
-		        controller: 'AppCtrl'
-	        })
-	        .state('app.activity',{
-		        url: '/activity',
-	            data: {
-	                authorizedRoles: [USER_ROLES.all]
-	            },
-		        views: {
-			        'menuContent':{
-				        templateUrl: 'templates/activity.html',
-				        controller: 'ActivityCtrl'
-			        },
-			        'fabContent': {
-				        template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced" ui-sref="app.post" ><i class="icon ion-plus"></i></button>',
-				        controller: function ($scope,$timeout,$ionicPopup) {
-							$scope.showPopup = function() {
-							    var alertPopup = $ionicPopup.alert({
-									title: 'Dont eat that!',
-									template: 'It might taste good'
-							    }); 
-							    $timeout(function () {
-									document.getElementById('fab-activity').classList.toggle('on');
-							    },200);
-							}}
-			        }
-		        }
-	        })
-	        .state('app.post',{
-		        url: '/post',
-		        views: {
-			        'menuContent':{
-				        templateUrl: 'templates/post.html',
-				        controller: 'PostCtrl'
-			        },
-			        'fabContent': {
-				        template:'<button id="fab-post" class="button button-fab button-fab-bottom-right button-balanced" ><i class="icon ion-plus"></i></button>',
-				        controller: function ($timeout) {
-							$timeout(function () {
-								document.getElementById('fab-post').classList.toggle('on');
-							}, 200);
-						}
-			        }
-		        }
-	        })
-	        .state('app.alert', {
-			    url: '/alert',
-			    views: {
-					'menuContent': {
-						templateUrl: 'templates/alert.html',
-						controller: 'AlertCtrl'
-					},
-					'fabContent': {
-						template:'<button id="fab-post" class="button button-fab button-fab-bottom-right button-balanced" ><i class="icon ion-plus"></i></button>',
-						controller: function ($timeout) {
-							$timeout(function () {
-								document.getElementById('fab-post').classList.toggle('on');
-							}, 200);
-						}
-					}
-			    }
-	        })
-	        .state('app.search', {
-			    url: '/search',
-			    views: {
-					'menuContent': {
-						templateUrl: 'templates/search.html',
-						controller: 'SearchCtrl'
-					}
-			    }
-	        })
-	        .state('signup', {
-		        url: '/signup',
-		        templateUrl: 'templates/signup.html',
-		        controller: 'SignupCtrl'
-	        })
-	        .state('app.intro', {
-		        url:'/intro',
-		        views: {
-			        'menuContent':{
-			            templateUrl: "templates/intro.html",
-			            controller:'IntroCtrl'
-			        }
-	            }
-	        })
-	        .state('app.setting',{
-		        url: '/setting',
-		        views: {
-			        'menuContent':{
-				        templateUrl: 'templates/setting.html',
-				        controller: 'SettingCtrl'
-			        },
-			        'fabContent': {
-				        template:'<button id="fab-setting" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
-				        controller: function ($timeout) {
-							$timeout(function () {
-								document.getElementById('fab-setting').classList.toggle('on');
-							}, 200);
-						}
-			        }
-		        }
-	        })
-	        .state('app.login', {
-		        url: '/login',
-		        views:{
-				    'menuContent': {
-						templateUrl: 'templates/login.html',
-						controller: 'LoginCtrl'
-				    },
-				    'fabContent': {
-					    template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
-					    controller: function ($timeout) {
-							$timeout(function () {
-								document.getElementById('fab-activity').classList.toggle('on');
-							}, 200);
-						}
-				    }
-		        }
-	        })
-	        .state('app.profile', {
-		        url: "/profile",
-		        views: {
-			        'menuContent': {
-				        templateUrl: "templates/profile.html",
-				        controller: 'ProfileCtrl'
-			        },
-			        'fabContent': {
-				        template:'',
-				        controller: ''
-			        }
-		        }
-	        })
-	        .state('app.messages', {
-		        url: '/messages',
-		        views: {
-			        'menuContent': {
-				        templateUrl: "templates/messages.html",
-				        controller: 'MessagesCtrl'
-			        },
-			        'fabContent': {
-				        template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
-				        controller: function ($timeout) {
-							$timeout(function () {
-								document.getElementById('fab-activity').classList.toggle('on');
-							}, 200);
-						}
-			        }
-		        }
-	        })
-	        .state('amessage', {
-		        url: '/amessage/:uid',
-		        templateUrl: 'templates/amessage.html',
-		        controller: 'AmessageCtrl'
-	        })
-	        .state('disease1', {
-		        url: '/disease',
-		        templateUrl: 'templates/disease.html',
-		        controller: 'DiseaseCtrl'
-	        })
-	        .state('medicine1', {
-		        url: '/medicine',
-		        templateUrl: 'templates/medicine.html',
-		        controller: 'MedicineCtrl'
-	        });
+        openFB.init({appId: APP_CONFIG.FACEBOOK_APP_ID});
+        $ionicConfigProvider.views.maxCache(0);
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+            .state('app.activity',{
+                url: '/activity',
+                data: {
+                    authorizedRoles: [USER_ROLES.all]
+                },
+                views: {
+                    'menuContent':{
+                        templateUrl: 'templates/activity.html',
+                        controller: 'ActivityCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced" ui-sref="app.post" ><i class="icon ion-plus"></i></button>',
+                        controller: function ($scope,$timeout,$ionicPopup) {
+                            $scope.showPopup = function() {
+                                var alertPopup = $ionicPopup.alert({
+                                    title: 'Dont eat that!',
+                                    template: 'It might taste good'
+                                }); 
+                                $timeout(function () {
+                                    document.getElementById('fab-activity').classList.toggle('on');
+                                },200);
+                            }}
+                    }
+                }
+            })
+            .state('app.post',{
+                url: '/post',
+                views: {
+                    'menuContent':{
+                        templateUrl: 'templates/post.html',
+                        controller: 'PostCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-post" class="button button-fab button-fab-bottom-right button-balanced" ><i class="icon ion-plus"></i></button>',
+                        controller: function ($timeout) {
+                            $timeout(function () {
+                                document.getElementById('fab-post').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
+                }
+            })
+            .state('app.alert', {
+                url: '/alert',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/alert.html',
+                        controller: 'AlertCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-post" class="button button-fab button-fab-bottom-right button-balanced" ><i class="icon ion-plus"></i></button>',
+                        controller: function ($timeout) {
+                            $timeout(function () {
+                                document.getElementById('fab-post').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
+                }
+            })
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html',
+                        controller: 'SearchCtrl'
+                    }
+                }
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'templates/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .state('app.intro', {
+                url:'/intro',
+                views: {
+                    'menuContent':{
+                        templateUrl: "templates/intro.html",
+                        controller:'IntroCtrl'
+                    }
+                }
+            })
+            .state('app.setting',{
+                url: '/setting',
+                views: {
+                    'menuContent':{
+                        templateUrl: 'templates/setting.html',
+                        controller: 'SettingCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-setting" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
+                        controller: function ($timeout) {
+                            $timeout(function () {
+                                document.getElementById('fab-setting').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
+                }
+            })
+            .state('app.login', {
+                url: '/login',
+                views:{
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
+                        controller: function ($timeout) {
+                            $timeout(function () {
+                                document.getElementById('fab-activity').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
+                }
+            })
+            .state('app.profile', {
+                url: "/profile",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/profile.html",
+                        controller: 'ProfileCtrl'
+                    },
+                    'fabContent': {
+                        template:'',
+                        controller: ''
+                    }
+                }
+            })
+            .state('app.messages', {
+                url: '/messages',
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/messages.html",
+                        controller: 'MessagesCtrl'
+                    },
+                    'fabContent': {
+                        template:'<button id="fab-activity" class="button button-fab button-fab-bottom-right button-balanced"><i class="icon ion-plus"></i></button>',
+                        controller: function ($timeout) {
+                            $timeout(function () {
+                                document.getElementById('fab-activity').classList.toggle('on');
+                            }, 200);
+                        }
+                    }
+                }
+            })
+            .state('amessage', {
+                url: '/amessage/:uid',
+                templateUrl: 'templates/amessage.html',
+                controller: 'AmessageCtrl'
+            })
+            .state('disease1', {
+                url: '/disease',
+                templateUrl: 'templates/disease.html',
+                controller: 'DiseaseCtrl'
+            })
+            .state('medicine1', {
+                url: '/medicine',
+                templateUrl: 'templates/medicine.html',
+                controller: 'MedicineCtrl'
+            });
 
         $urlRouterProvider.otherwise( function( $injector ) {
             var $state = $injector.get('$state');
--- dr.near/www/js/services.js
+++ dr.near/www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('DrNEAR.services',['ngResource'])	
+angular.module('DrNEAR.services',['ngResource'])    
     .run(function ($rootScope, $state, AUTH_EVENTS, AuthService) {
         $rootScope.$on( '$stateChangeStart', function( event, toState, toParam, fromState, fromParam ) {
             if ( typeof(toState.data) == 'object' ) {
@@ -46,61 +46,61 @@ angular.module('DrNEAR.services',['ngResource'])
         };
         return authService;
     })
-	.factory( 'LoginUser', function() {
-		console.log( 'LoginUser' );
-	  	// ログインユーザーは仮に「最初のユーザー」とする	  
-		var UserObject = Parse.Object.extend( 'User2' );
-		var query = new Parse.Query( UserObject );
-		query.limit(1);
-		query.ascending( 'createdAt' );
-		return query.first().then( function( result ) 
-		                           { return result.fetch();  } );
-	})
- 	.service('Session', function () {
-		console.log( 'Session' );
-		this.create = function (sessionId, userId, userRole) {
-		    this.id = sessionId;
-		    this.userId = userId;
-		    this.userRole = userRole;
-		};
-	 	this.destroy = function () {
-		    this.id = null;
-		    this.userId = null;
-		    this.userRole = null;
-	 	};
-  	})
-  	.constant('AUTH_EVENTS', {
-		loginSuccess     : 'auth-login-success',
-		loginFailed      : 'auth-login-failed',
-		logoutSuccess    : 'auth-logout-success',
-		sessionTimeout   : 'auth-session-timeout',
-		notAuthenticated : 'auth-not-authenticated',
-		notAuthorized    : 'auth-not-authorized'
-   	})
-  	.constant('USER_ROLES', {
-		all    : '*',
-		admin  : 'admin',
-		editor : 'editor',
-		guest  : 'guest'
-  	})
-	.directive('formAutofillFix', function ($timeout) {
-		console.log( 'formAutofillFix' ); 
-		return function (scope, element, attrs) {
-		    element.prop('method', 'post');
-		    if (attrs.ngSubmit) {
-		        $timeout(function () {
-			        element
-			            .unbind('submit')
-			            .bind('submit', function (event) {
-				            event.preventDefault();
-				            element
-				                .find('input, textarea, select')
-				                .trigger('input')
-				                .trigger('change')
-				                .trigger('keydown');
-				            scope.$apply(attrs.ngSubmit);
-			            });
-		        });
-		    }
-		};
-	});
+    .factory( 'LoginUser', function() {
+        console.log( 'LoginUser' );
+        // ログインユーザーは仮に「最初のユーザー」とする     
+        var UserObject = Parse.Object.extend( 'User2' );
+        var query = new Parse.Query( UserObject );
+        query.limit(1);
+        query.ascending( 'createdAt' );
+        return query.first().then( function( result ) 
+                                   { return result.fetch();  } );
+    })
+    .service('Session', function () {
+        console.log( 'Session' );
+        this.create = function (sessionId, userId, userRole) {
+            this.id = sessionId;
+            this.userId = userId;
+            this.userRole = userRole;
+        };
+        this.destroy = function () {
+            this.id = null;
+            this.userId = null;
+            this.userRole = null;
+        };
+    })
+    .constant('AUTH_EVENTS', {
+        loginSuccess     : 'auth-login-success',
+        loginFailed      : 'auth-login-failed',
+        logoutSuccess    : 'auth-logout-success',
+        sessionTimeout   : 'auth-session-timeout',
+        notAuthenticated : 'auth-not-authenticated',
+        notAuthorized    : 'auth-not-authorized'
+    })
+    .constant('USER_ROLES', {
+        all    : '*',
+        admin  : 'admin',
+        editor : 'editor',
+        guest  : 'guest'
+    })
+    .directive('formAutofillFix', function ($timeout) {
+        console.log( 'formAutofillFix' ); 
+        return function (scope, element, attrs) {
+            element.prop('method', 'post');
+            if (attrs.ngSubmit) {
+                $timeout(function () {
+                    element
+                        .unbind('submit')
+                        .bind('submit', function (event) {
+                            event.preventDefault();
+                            element
+                                .find('input, textarea, select')
+                                .trigger('input')
+                                .trigger('change')
+                                .trigger('keydown');
+                            scope.$apply(attrs.ngSubmit);
+                        });
+                });
+            }
+        };
+    });
